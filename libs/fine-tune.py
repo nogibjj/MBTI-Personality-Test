c9e8bf9fcaa19608c9e690d2d4682ae377474b13
Hank <40827070+Shunian-Chen@users.noreply.github.com>
2022-12-16 01:07:20 +0000

    fine tune script

--- libs/fine-tune.py
+++ libs/fine-tune.py
@@ -14,12 +14,12 @@ from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer
 import numpy as np
 
-MODEL_PATH = "bert-base-uncased"
-TOKENIZER_PATH = "bert-base-uncased"
-CHECK_POINT_PATH = "/workspaces/MBTI-Personality-Test/mbti-classification/checkpoint-45000"
-OUTPUT_DIR = "/workspaces/MBTI-Personality-Test/mbti-classification"
+MODEL_PATH = "roberta-base"
+TOKENIZER_PATH = "roberta-base"
+CHECK_POINT_PATH = "/workspaces/MBTI-Personality-Test/mbti-classification-roberta-base/checkpoint-20000"
+OUTPUT_DIR = "/workspaces/MBTI-Personality-Test/mbti-classification-roberta-base"
 DATA_PATH = "/workspaces/MBTI-Personality-Test/tokenized_datasets"
-REPO_DIR = "mbti-classification-bert-base-uncased"
+REPO_DIR = "mbti-classification-roberta-base"
 
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=265)
@@ -39,11 +39,20 @@ else:
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=16, ignore_mismatched_sizes=True)
 tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
 
-metric = load("accuracy")
+
 def compute_metrics(eval_pred):
+    metric1 = load("accuracy")
+    # metric2 = load("precision")
+    # metric3 = load("recall")
+
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+
+    accuracy = metric1.compute(predictions=predictions, references=labels)['accuracy']
+    # precision = metric2.compute(predictions=predictions, references=labels)['precision']
+    # recall = metric3.compute(predictions=predictions, references=labels)['recall']
+
+    return {"accuracy": accuracy}
 
 # can use if needed to reduce memory usage and training time
 small_train_dataset = tokenized_datasets["train"].shuffle(seed=42)
@@ -55,15 +64,20 @@ training_args = TrainingArguments(  output_dir=OUTPUT_DIR,
                                     save_strategy="steps",
                                     save_steps=5000,
                                     save_total_limit=5, 
-                                    per_device_train_batch_size=24, 
-                                    per_device_eval_batch_size=24, 
+                                    per_device_train_batch_size=16, 
+                                    per_device_eval_batch_size=16, 
                                     learning_rate=2e-5, 
                                     num_train_epochs=5, 
                                     weight_decay=0.01,
                                     lr_scheduler_type='cosine',
                                     push_to_hub=True,
                                     hub_model_id=REPO_DIR,
-                                    hub_strategy="every_save")
+                                    hub_strategy="every_save",
+                                    logging_dir=OUTPUT_DIR + "/runs",
+                                    logging_steps=100,
+                                    logging_strategy="steps",
+                                    logging_first_step=True,
+                                    report_to="tensorboard",)
 trainer = Trainer(
     model=model,
     args=training_args,
